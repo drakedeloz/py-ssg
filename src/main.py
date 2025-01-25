c41d991ab5f70182b82d7bcd14dd5427c2a636cd
Drake Delozier <drakedeloz@gmail.com>
2025-01-25 14:49:07 -0600

    TextType and TextNode

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import *
+
+def main():
+    text = TextNode("This is a text node", TextType("bold"), "https://deloz.dev")
+    print(text)
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = "normal"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK_TEXT = "link"
+    IMAGE_TEXT = "image"
+
+class TextNode():
+    def __init__(self, text, text_type: TextType, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, target_node):
+        return (
+            self.text == target_node.text and
+            self.text_type == target_node.text_type and
+            self.url == target_node.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
